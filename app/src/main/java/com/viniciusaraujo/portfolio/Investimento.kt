71b95ece3d29949f6f38d4c478a3d86c2d4606d9
sebastianbavini <sebastianbavini@hotmail.com>
2022-06-26 20:57:00 -0300

    versao que deu origem ao projeto IchangeBitSpeed

--- app/src/main/java/com/viniciusaraujo/portfolio/Investimento.kt
+++ app/src/main/java/com/viniciusaraujo/portfolio/Investimento.kt
@@ -7,7 +7,7 @@ class Investimento {
     private var nome:String=""
     private var acumulado: Double=0.0
     private var liquido:Double=0.0
-    private var precoDeCompra:Double=0.00
+    private var precoDeCompra:Double=0.0
     private var precoDeVenda:Double=0.0
     private var lucro:Double=0.0
     private var pontos:Double=0.0
@@ -15,12 +15,24 @@ class Investimento {
     private var diasDeTrabalho:Double=0.0
     private var horasDeTrabalho:Double=0.0
     private var precoDaHora:Double=0.0
+    private var qtdLitrosComprados:Double=0.0
+    private var precoDoLitro:Double=0.0
+    private var autonomiaDoVeiculo:Double=0.1
+    private var distanciaPercorrida:Double=0.0
+    private var pensaoAlimenticia:Double=1200.0
 
     fun setNome(novo:String){
         this.nome=novo
     }
-    fun setAcumulado(valor:Double){
+    /**fun setAcumulado(valor:Double){
         this.acumulado=valor
+    } */
+    fun calculaTotais(){
+        precoDeVenda= diasDeTrabalho*horasDeTrabalho*precoDaHora
+        precoDeCompra=qtdLitrosComprados*precoDoLitro*distanciaPercorrida/autonomiaDoVeiculo
+        this.acumulado=precoDeCompra-precoDeVenda
+        this.liquido=precoDeVenda-precoDeCompra-pensaoAlimenticia
+
     }
     fun setLiquido(vl:Double){
         this.liquido=vl
@@ -49,4 +61,61 @@ class Investimento {
     fun setPrecoDaHora(preco:Double){
         this.precoDaHora=preco
     }
+
+
+    fun getNome():String{
+        return this.nome
+    }
+    fun getAcumulado():Double{
+        return this.acumulado
+    }
+    fun getLiquido():Double{
+        return this.liquido
+    }
+    fun getPrecoDeCompra():Double{
+        return this.precoDeCompra
+    }
+    fun getPrecoDeVenda():Double{
+        return this.precoDeVenda
+    }
+    fun getLucro():Double{
+        return this.lucro
+    }
+    fun getPontos():Double{
+        return this.pontos
+    }
+
+        fun getMilhas():Double{
+            return this.milhas
+        }
+    fun getDiasDeTrabalho():Double{
+        return this.diasDeTrabalho
+    }
+    fun getHorasDeTrabalho():Double{
+        return this.horasDeTrabalho
+    }
+    fun getPrecoDaHora():Double{
+        return this.precoDaHora
+    }
+
+    fun printInvestimento(){
+
+        println("Identificação do Investimento: ${this.getNome()}")
+        println("Total Acumulado: ${this.getAcumulado()}")
+        println("Total de Dinheiro Líquido: ${this.getLiquido()}")
+        print("Total de Milhas: ${this.getMilhas()}")
+        println("O Lucro Total foi de:${this.getLucro()}")
+        println("Total de horas Trabalhadas: ${this.getDiasDeTrabalho()}")
+        println("Total de Pontos no cartao de crédito ${this.getPontos()}")
+        println("Total de dias Trabalhados: ${this.getDiasDeTrabalho()}")
+        println("Total de Horas de Trabalho ${this.getHorasDeTrabalho()}")
+        println("Preço da Hora de Trabalho: ${this.getPrecoDaHora()}")
+        println("Preço de Compra (valor aportado) : ${this.getPrecoDeCompra()}")
+        println("Preço de Venda do Investimento ${this.getPrecoDeVenda()}")
+
+
+    }
+
 }
+
+
--- app/src/main/java/com/viniciusaraujo/portfolio/MainActivity.kt
+++ app/src/main/java/com/viniciusaraujo/portfolio/MainActivity.kt
@@ -2,10 +2,11 @@ package com.viniciusaraujo.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import com.viniciusaraujo.portfolio.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pontos: EditText
     private lateinit var milhas: EditText
     private lateinit var horasDeTrabalho: EditText
-    private lateinit var diasDeTrabalho:EditText
-    private lateinit var precoDaHora:EditText
+    private lateinit var diasDeTrabalho: EditText
+    private lateinit var precoDaHora: EditText
     private lateinit var trajetoEmKMs: EditText
     private lateinit var kmPorLitro: EditText
     private lateinit var qtdLitros: EditText
@@ -28,42 +29,75 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        nome=binding.editTextNome
-        acumulado=binding.editTextAcumulado
-        liquido=binding.editTextLiquido
-        precoCompra=binding.editTextPrecoDeCompra
-        precoVenda=binding.editTextPrecoDeVenda
-        lucro=binding.editTextLucro
-        pontos=binding.editTextPontos
-        milhas=binding.editTextMilhas
-        horasDeTrabalho=binding.editTextHorasDeTrabalho
-        diasDeTrabalho=binding.editTextDiasDeTrabalho
-        precoDaHora=binding.editTextPrecoHora
-        trajetoEmKMs=binding.editTextTrajetoKms
-        kmPorLitro=binding.editTextKmForLiter
-        qtdLitros=binding.editTextLitrosComprados
-        precoPorLitro=binding.editTextPrecoPorLitro
-        qtdUnidades=binding.editTextQtdUnidades
+        nome = binding.editTextNome
+        acumulado = binding.editTextAcumulado
+        liquido = binding.editTextLiquido
+        precoCompra = binding.editTextPrecoDeCompra
+        precoVenda = binding.editTextPrecoDeVenda
+        lucro = binding.editTextLucro
+        pontos = binding.editTextPontos
+        milhas = binding.editTextMilhas
+        horasDeTrabalho = binding.editTextHorasDeTrabalho
+        diasDeTrabalho = binding.editTextDiasDeTrabalho
+        precoDaHora = binding.editTextPrecoHora
+        trajetoEmKMs = binding.editTextTrajetoKms
+        kmPorLitro = binding.editTextKmForLiter
+        qtdLitros = binding.editTextLitrosComprados
+        precoPorLitro = binding.editTextPrecoPorLitro
+        qtdUnidades = binding.editTextQtdUnidades
+        binding.imageSave.setOnClickListener(this)
+
+        setContentView(binding.root)
 
 
+    }
+
+    override fun onClick(p: View) {
+        if (p.id == R.id.image_save) {
+            println("Ouvindo...")
+            salvar()
+        }
     }
 
     private fun salvar() {
 
         var invest = Investimento()
-        invest.setNome(nome.toString())
-        invest.setAcumulado(acumulado.toString().toDouble())
-        invest.setLiquido(liquido.toString().toDouble())
-        invest.setPrecoDeCompra(precoCompra.toString().toDouble())
-        invest.setPrecoDeVenda(precoVenda.toString().toDouble())
-        invest.setLucro(lucro.toString().toDouble())
-        invest.setPontos(pontos.toString().toDouble())
-        invest.setMilhas(milhas.toString().toDouble())
-        invest.setHorasDeTrabalho(horasDeTrabalho.toString().toDouble())
+
+        invest.setNome(nome.text.toString())
+
+
+        if (precoCompra.text.toString() != "") {
+            invest.setPrecoDeCompra(precoCompra.text.toString().toDouble())
+        }
+        if (precoVenda.text.toString() != "")
+            invest.setPrecoDeVenda(precoVenda.text.toString().toDouble())
+        if (lucro.text.toString() != "")
+            invest.setLucro(lucro.text.toString().toDouble())
+        if (pontos.text.toString() != "")
+            invest.setPontos(pontos.text.toString().toDouble())
+        if (milhas.text.toString() != "")
+            invest.setMilhas(milhas.text.toString().toDouble())
+        if (diasDeTrabalho.text.toString()!="")
+            invest.setDiasDeTrabalho(diasDeTrabalho.text.toString().toDouble())
+        if (horasDeTrabalho.text.toString() != "")
+            invest.setHorasDeTrabalho(horasDeTrabalho.text.toString().toDouble())
+
+        if (precoDaHora.text.toString() != "")
+            invest.setPrecoDaHora(precoDaHora.text.toString().toDouble())
+
+
+        invest.calculaTotais()
+        acumulado.setText(invest.getAcumulado().toString())
+        liquido.setText(invest.getLiquido().toString())
+
+
+        invest.printInvestimento()
     }
+
+
 }
